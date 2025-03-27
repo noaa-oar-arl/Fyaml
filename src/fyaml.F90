329170df674ac444efb6893278f08f00e2eeadfe
zmoon <zachary.moon@noaa.gov>
2025-03-27 16:11:14 +0000

    Fix for ifort

--- src/fyaml.F90
+++ src/fyaml.F90
@@ -1250,7 +1250,7 @@ contains
         ! Allocate array for keys
         if (count > 0) then
             allocate(character(len=32) :: keys(count))
-            keys = ""
+            keys(:) = ""
 
             ! Fill array with root keys
             current => this%docs(1)%first
